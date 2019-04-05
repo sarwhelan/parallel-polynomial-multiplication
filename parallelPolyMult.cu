8f6a7214f9abcc5d6a6ca80a01e46723ce8dcae7
Sarah Whelan <sarwhelan11@gmail.com>
2019-04-05 15:52:22 -0400

    add print statements to parallel mult

--- parallelPolyMult.cu
+++ parallelPolyMult.cu
@@ -194,9 +194,9 @@ __global__ void multPolynomialsParallel(int *polyA, int *polyB, int *product, in
     int a = blockIdx.x; // n blocks means each block has a corresponding term in A
     int b = (blockIdx.x + threadIdx.x) % polySize; // each thread assigned to term in B
     int degreeOfTerms = a + b;
-    printf("I am block=%d, thread=%d and the val of product[%] is %d. adding %d * %d = %d, mod is %d", blockIdx.x, threadIdx.x, degreeOfTerms, polyA[a], polyB[b], polyA[a]*polyB[b], (polyA[a]*polyB[b])%modBy);
+    printf("I am block=%d, thread=%d and the val of product[%] is %d. adding %d * %d = %d, mod is %d\n\n", blockIdx.x, threadIdx.x, degreeOfTerms, polyA[a], polyB[b], polyA[a]*polyB[b], (polyA[a]*polyB[b])%modBy);
     product[degreeOfTerms] = (product[a*b] + polyA[a] * polyB[b]) % modBy; 
-    printf("I am block=%d, thread=%d and now prouct[%d] is %d", blockIdx.x, threadIdx.x, degreeOfTerms, product[degreeOfTerms]);
+    printf("I am block=%d, thread=%d and now prouct[%d] is %d\n\n", blockIdx.x, threadIdx.x, degreeOfTerms, product[degreeOfTerms]);
 }
 
 void checkCUDAError(const char *msg)
